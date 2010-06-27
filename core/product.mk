e5214a4844fd9628e95a7bf77dc5c451586bfbe8
Koushik Dutta <koushd@gmail.com>
2010-06-26 22:53:55 -0700

    Add support for overriding build.prop settings
    Change-Id: Ie4224d608b7bcb0c2087bd77bc954f8f5f9b6495

--- core/product.mk
+++ core/product.mk
@@ -117,6 +117,7 @@ _product_list_vars :=
 _product_single_value_vars += PRODUCT_NAME
 _product_single_value_vars += PRODUCT_MODEL
 
+_product_list_vars += PRODUCT_BUILD_PROP_OVERRIDES
 # The resoure configuration options to use for this product.
 _product_list_vars += PRODUCT_LOCALES
 _product_list_vars += PRODUCT_AAPT_CONFIG
