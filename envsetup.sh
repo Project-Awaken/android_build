f26608782a2167dcda2cef7e868fea6872aa9fa2
Emilio López <turl@tuxfamily.org>
2013-11-03 13:02:13 -0300

    Do not print an error if complete fails
    complete is a bashism; so it will fail on other shells. Avoid printing
    an error if that is the case.
    
    Change-Id: Id6d6311792f409cc3a697c7a2bb003863f1afe60

--- envsetup.sh
+++ envsetup.sh
@@ -396,7 +396,7 @@ function addcompletions()
         # Doesn't work in zsh.
         complete -o nospace -F _croot croot
     fi
-    complete -F _lunch lunch
+    complete -F _lunch lunch 2>/dev/null
 
     complete -F _complete_android_module_names gomod
     complete -F _complete_android_module_names m
