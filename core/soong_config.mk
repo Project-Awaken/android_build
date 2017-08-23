bc08acc4716c19a3803a2aaafb389acf210d909c
Simon Shields <simon@lineageos.org>
2017-08-23 12:06:18 +1000

    soong: Include Awaken board flags
    Change-Id: I1aabe4915036790dd35ceb985cf0a2701ca0faf9

--- core/soong_config.mk
+++ core/soong_config.mk
@@ -22,6 +22,8 @@ ifeq ($(WRITE_SOONG_VARIABLES),true)
 $(shell mkdir -p $(dir $(SOONG_VARIABLES)))
 $(call json_start)
 
+include vendor/awaken/build/soong/soong_config.mk
+
 $(call add_json_str,  Make_suffix, -$(TARGET_PRODUCT))
 
 $(call add_json_str,  BuildId,                           $(BUILD_ID))
