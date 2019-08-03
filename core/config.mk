c80ca60a4a81fc24db40977e81d30850dbcab9b4
Stefen Wakefield <xstefen@gmail.com>
2019-08-03 11:38:03 -0500

    make: we should include some policy in the case that we're doing a real build
    Signed-off-by: Stefen Wakefield <xstefen@gmail.com>
    
    Change-Id: Iede5830093497abe753a34df3bc4913468be39d2

--- core/config.mk
+++ core/config.mk
@@ -1245,6 +1245,12 @@ endif
 DEFAULT_DATA_OUT_MODULES := ltp $(ltp_packages) $(kselftest_modules)
 .KATI_READONLY := DEFAULT_DATA_OUT_MODULES
 
+ifneq ($(AWAKEN_BUILD),)
+## We need to be sure the global selinux policies are included
+## last, to avoid accidental resetting by device configs
+$(eval include device/awaken/sepolicy/common/sepolicy.mk)
+endif
+
 include $(BUILD_SYSTEM)/dumpvar.mk
 
 # Include any vendor specific config.mk file
