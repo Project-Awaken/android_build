dfbba7c6327b9e06febc260c64e82b29f04a20c9
Rashed Abdel-Tawab <rashed@linux.com>
2019-09-05 21:35:38 -0700

    core: Only include Awaken SE policy if repo exists
    Change-Id: If799106d2da6d8c303430e316edff365a22f7c99

--- core/config.mk
+++ core/config.mk
@@ -1177,9 +1177,11 @@ DEFAULT_DATA_OUT_MODULES := ltp $(ltp_packages) $(kselftest_modules)
 RECORD_ALL_DEPS :=$= $(filter true,$(RECORD_ALL_DEPS))
 
 ifneq ($(AWAKEN_BUILD),)
+ifneq ($(wildcard device/awaken/sepolicy/common/sepolicy.mk),)
 ## We need to be sure the global selinux policies are included
 ## last, to avoid accidental resetting by device configs
 $(eval include device/awaken/sepolicy/common/sepolicy.mk)
+endif
 endif
 
 # Include any vendor specific config.mk file
