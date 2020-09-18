abd0938af779fe2e896e175e3de9a1c36ed6cbbb
Danny Lin <danny@kdrag0n.dev>
2020-09-18 11:45:52 -0700

    Don't build QuickSearchBox
    We don't want this ancient search box from Android 2.x.
    
    Change-Id: Iaaf4623971d58e6eab6125a07a05f4c36c60fe79

--- target/product/handheld_product.mk
+++ target/product/handheld_product.mk
@@ -30,7 +30,6 @@ PRODUCT_PACKAGES += \
     Gallery2 \
     Music \
     preinstalled-packages-platform-handheld-product.xml \
-    QuickSearchBox \
     SettingsIntelligence \
     frameworks-base-overlays
 
