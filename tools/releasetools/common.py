100a8443a155cedbc7bb79ef54139c3764727da5
Sukrut4778 <sukrutbhagwat4778@gmail.com>
2020-11-07 19:07:45 +0530

    releasetools: don't remove other realme dynamic partitions
    * Building custom vendor for realme dynamic partition devices removes all realme specific partitions
    which results into bootstuck or random bootloop so don't remove realme shit partitions
    
    * set BUILD_REALME_VENDOR := true in BoardConfig.mk
    
    Signed-off-by: Sukrut4778 <sukrutbhagwat4778@gmail.com>
    Signed-off-by: Joey Huab <joey@evolution-x.org>

--- tools/releasetools/common.py
+++ tools/releasetools/common.py
@@ -3175,11 +3175,12 @@ class DynamicGroupUpdate(object):
 
 class DynamicPartitionsDifference(object):
   def __init__(self, info_dict, block_diffs, progress_dict=None,
-               source_info_dict=None, build_without_vendor=False):
+               source_info_dict=None, build_without_vendor=False, build_realme_vendor=False):
     if progress_dict is None:
       progress_dict = {}
 
     self._build_without_vendor = build_without_vendor
+    self._build_realme_vendor = build_realme_vendor
     self._remove_all_before_apply = False
     if source_info_dict is None:
       self._remove_all_before_apply = True
@@ -3307,6 +3308,17 @@ class DynamicPartitionsDifference(object):
     if self._build_without_vendor:
       comment('System-only build, keep original vendor partition')
       # When building without vendor, we do not want to override
+      # any partition already existing. In this case, we can only
+      # resize, but not remove / create / re-create any other
+      # partition.
+      for p, u in self._partition_updates.items():
+        comment('Resize partition %s to %s' % (p, u.tgt_size))
+        append('resize %s %s' % (p, u.tgt_size))
+      return
+
+    if self._build_realme_vendor:
+      comment('Keep original realme partitions')
+      # When building realme vendor, we do not want to override
       # any partition already existing. In this case, we can only
       # resize, but not remove / create / re-create any other
       # partition.
