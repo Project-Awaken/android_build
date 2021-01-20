2e855b45d522aeb179f31267287d19b30298fdcb
saikiran2001 <bjsaikiran@gmail.com>
2021-01-20 13:07:36 +0100

    fixup! make: we should include some policy in the case that
    Signed-off-by: saikiran2001 <bjsaikiran@gmail.com>

--- core/config.mk
+++ core/config.mk
@@ -1252,9 +1252,11 @@ DEFAULT_DATA_OUT_MODULES := ltp $(ltp_packages) $(kselftest_modules)
 .KATI_READONLY := DEFAULT_DATA_OUT_MODULES
 
 ifneq ($(AWAKEN_BUILD),)
+ifneq ($(wildcard device/awaken/sepolicy/common/sepolicy.mk),)
 ## We need to be sure the global selinux policies are included
 ## last, to avoid accidental resetting by device configs
 $(eval include device/awaken/sepolicy/common/sepolicy.mk)
+endif
 endif
 
 include $(BUILD_SYSTEM)/dumpvar.mk
