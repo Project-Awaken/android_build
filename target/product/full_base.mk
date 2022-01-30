058d50f685a0d5cdce5fb9c842821d08edec8c8a
ghostrider-reborn <gh0strider.2k18.reborn@gmail.com>
2022-01-31 01:30:26 +0600

    target: Do not include AOSP sounds
    * they are overriding our pixel ones
    
    Change-Id: Id2d80e06359399b1cb86e9a4b952c7c9eccb603f

--- target/product/full_base.mk
+++ target/product/full_base.mk
@@ -50,13 +50,6 @@ PRODUCT_VENDOR_PROPERTIES := \
 # Put en_US first in the list, so make it default.
 PRODUCT_LOCALES := en_US
 
-# Get some sounds
-ifeq ($(AWAKEN_BUILD),)
-$(call inherit-product-if-exists, frameworks/base/data/sounds/AllAudio.mk)
-else
-$(call inherit-product-if-exists, frameworks/base/data/sounds/AudioPackage14.mk)
-endif
-
 # Get a list of languages.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
 
